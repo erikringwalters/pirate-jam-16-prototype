c63f7a3a4951a591d89ae105339427911a33e4b5
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-19 22:48:05 -0800

    Enemy weapon spawning (rough)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,4 +1,11 @@
 extends RigidBody3D
 
+@onready var _weapon_marker = %WeaponMarker
+
+func _ready() -> void:
+	var weapon = Items.weapons["Sword"].instantiate()
+	add_child(weapon)
+	weapon.global_transform.origin = _weapon_marker.global_transform.origin
+
 func _physics_process(_delta: float) -> void:
 	pass
--- scripts/items.gd
+++ scripts/items.gd
@@ -1,13 +1,19 @@
 extends Node
 
-var weapons := ["Dagger", "Sword", "Handgun", "Shotgun", "Rocket"]
-var weapon_scenes := []
+var weapons : Dictionary = {
+	"Dagger" : "", 
+	"Sword" : "", 
+	"Handgun" : "", 
+	"Shotgun" : "", 
+	"Rocket" : ""
+}
 
-var projectiles := ["Bullet"]
+var projectiles : Dictionary = {
+	"Bullet" : "res://scenes/weapons/bullet.tscn"
+}
 
-@onready var projectile_scene : PackedScene = preload("res://scenes/weapons/bullet.tscn")
+@onready var projectile_scene : PackedScene = ResourceLoader.load(projectiles["Bullet"])
 
 func _ready() -> void:
-	for weapon in weapons:
-		weapon_scenes.push_back(ResourceLoader.load("res://assets/weapons/" + weapon + ".fbx"))
-		
+	for weapon in weapons.keys():
+		weapons[weapon] = ResourceLoader.load("res://assets/weapons/" + weapon + ".fbx")
