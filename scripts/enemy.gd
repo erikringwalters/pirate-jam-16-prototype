81f8fd03808c2b7119962a89a646940d4d8a1093
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-21 00:17:23 -0800

    minor cleanup

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -11,15 +11,14 @@ func _ready() -> void:
 	weapon.global_transform.origin = _weapon_marker.global_transform.origin
 
 func _physics_process(delta: float) -> void:
+	# Look at player # TODO: Slowly look toward player to give player a chance to avoid/kite
 	look_at(get_parent().get_node("Player").global_transform.origin)
-	var forward := global_basis.z
-	print(forward)
-	var move_direction := forward
+	
+	# Move toward player
+	var move_direction := global_basis.z
 	move_direction.y = 0.0
 	move_direction = -move_direction.normalized() * move_direction.length()
-	
 	var vel = linear_velocity.move_toward(move_direction * move_speed, acceleration * delta)
-	
 	linear_velocity.x = clamp(vel.x, -move_speed, move_speed)
 	linear_velocity.y = 0.0
 	linear_velocity.z = clamp(vel.z, -move_speed, move_speed)
