6d892b4dbeb043e237d275ba7d3958b871a49193
Erik Ring-Walters <erik.ring.walters@gmail.com>
2025-01-27 22:08:38 -0800

    basic spawn new wave logic (wip)

--- scripts/level.gd
+++ scripts/level.gd
@@ -7,29 +7,32 @@ extends Node3D
 @export var y_offset : float = 1.0
 @export var z_offset : float = 20.0
 @export var spread_offset : float = 5
+@export var wave_n := [1,2,2,3,3,4,5]
 
 var enemies_remaining = 0
 
-@onready var enemies_remaining_timer = %EnemyCountTimer
+@onready var enemy_count_timer = %EnemyCountTimer
+@onready var wave_timer = %WaveTimer
 @onready var ui = $UI
 
 func _ready() -> void:
-	enemies_remaining_timer.wait_time = 0.1
-	spawn_wave(2)
+	enemy_count_timer.wait_time = 0.1
+	spawn_wave(wave_n[GameState.current_wave])
 
 # n will be squared
 func spawn_wave(n:int) -> void:
-	for i in n:
-		for j in n:
-			# Spawn enemy
-			var enemy = enemy_scene.instantiate()
-			add_child(enemy)
-			enemies_remaining += 1
-			enemy.global_transform.origin = Vector3(
-				i * spread_offset + x_offset,
-				y_offset, 
-				j * spread_offset + z_offset
-			)
+	if n <= 7: # change to max wave count
+		for i in n:
+			for j in n:
+				# Spawn enemy
+				var enemy = enemy_scene.instantiate()
+				add_child(enemy)
+				enemies_remaining += 1
+				enemy.global_transform.origin = Vector3(
+					i * spread_offset + x_offset,
+					y_offset, 
+					j * spread_offset + z_offset
+				)
 
 func _on_enemy_died():
 	enemies_remaining -= 1
@@ -37,17 +40,19 @@ func _on_enemy_died():
 	ui.update_score()
 	print(enemies_remaining, " enemies left")
 	if enemies_remaining <= 0:
-		enemies_remaining_timer.start()
+		enemy_count_timer.start()
 
-		
-func wave_over():
-	print("wave over")
-	print("new wave starting")
-	spawn_wave(5)
-
-
-func _on_enemies_remaining_timer_timeout() -> void:
+func _on_enemy_count_timer_timeout() -> void:
 	#verify enemy count in case something went wrong
 	enemies_remaining = get_tree().get_node_count_in_group("Enemy")
 	if enemies_remaining <= 0:
 		wave_over()
+
+func wave_over():
+	print("wave over")
+	wave_timer.start()
+
+func _on_wave_timer_timeout() -> void:
+	print("new wave starting")
+	GameState.current_wave += 1
+	spawn_wave(GameState.current_wave)
